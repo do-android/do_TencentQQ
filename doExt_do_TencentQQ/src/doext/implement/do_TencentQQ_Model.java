d20c682e7a2d6034db70910f69d05af38cef1576
haofs <haofengshang@126.com>
2016-04-22 17:48:39 +0800

    【Android】商店组件，qq分享成功后，demo中写了输出返回值和返回值类型，实际没有返回true和Boolean http://jira.deviceone.net:8088/browse/PRODDEV-2316

--- doExt_do_TencentQQ/src/doext/implement/do_TencentQQ_Model.java
+++ doExt_do_TencentQQ/src/doext/implement/do_TencentQQ_Model.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.tencent.connect.UserInfo;
+import com.tencent.connect.common.Constants;
 import com.tencent.connect.share.QQShare;
 import com.tencent.connect.share.QzoneShare;
 import com.tencent.open.utils.ThreadManager;
@@ -20,6 +22,8 @@ import com.tencent.tauth.UiError;
 import core.DoServiceContainer;
 import core.helper.DoIOHelper;
 import core.helper.DoJsonHelper;
+import core.interfaces.DoActivityResultListener;
+import core.interfaces.DoIPageView;
 import core.interfaces.DoIScriptEngine;
 import core.object.DoInvokeResult;
 import core.object.DoSingletonModule;
@@ -33,13 +37,17 @@ import doext.define.do_TencentQQ_IMethod;
  * 参数解释：@_messageName字符串事件名称，@jsonResult传递事件参数对象； 获取DoInvokeResult对象方式new
  * DoInvokeResult(this.getUniqueKey());
  */
-public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQQ_IMethod {
-
+public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQQ_IMethod, DoActivityResultListener {
 	private Tencent mTencent;
+	private DoIPageView activity;
+	private DoIScriptEngine doIScriptEngine;
+	private String callbackFuncName;
+	private DoActivityResultListener activityResultListener;
 
 	public do_TencentQQ_Model() throws Exception {
 		super();
 		do_TencentQQ_App.getInstance().setModuleTypeID(getTypeID());
+		activityResultListener = this;
 	}
 
 	/**
@@ -80,10 +88,10 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 		} else if ("logout".equals(_methodName)) {
 			this.logout(_dictParas, _scriptEngine, _callbackFuncName);
 			return true;
-		}else if ("shareToQQ".equals(_methodName)) {
+		} else if ("shareToQQ".equals(_methodName)) {
 			this.shareToQQ(_dictParas, _scriptEngine, _callbackFuncName);
 			return true;
-		}else if ("shareToQzone".equals(_methodName)) {
+		} else if ("shareToQzone".equals(_methodName)) {
 			this.shareToQzone(_dictParas, _scriptEngine, _callbackFuncName);
 			return true;
 		}
@@ -99,9 +107,9 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 	 */
 	@Override
 	public void getUserInfo(JSONObject _dictParas, DoIScriptEngine _scriptEngine, String _callbackFuncName) throws Exception {
-		String _token = DoJsonHelper.getString(_dictParas,"token", "");
-		String _expires = DoJsonHelper.getString(_dictParas,"expires", "");
-		String _openId = DoJsonHelper.getString(_dictParas,"openId", "");
+		String _token = DoJsonHelper.getString(_dictParas, "token", "");
+		String _expires = DoJsonHelper.getString(_dictParas, "expires", "");
+		String _openId = DoJsonHelper.getString(_dictParas, "openId", "");
 		Activity _activity = DoServiceContainer.getPageViewFactory().getAppContext();
 		if (mTencent != null && mTencent.isSessionValid() && !TextUtils.isEmpty(_token) && !TextUtils.isEmpty(_expires) && !TextUtils.isEmpty(_openId)) {
 			mTencent.setAccessToken(_token, _expires);
@@ -164,7 +172,7 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 	 */
 	@Override
 	public void login(JSONObject _dictParas, DoIScriptEngine _scriptEngine, String _callbackFuncName) throws Exception {
-		String _appId = DoJsonHelper.getString(_dictParas,"appId", "");
+		String _appId = DoJsonHelper.getString(_dictParas, "appId", "");
 		if (TextUtils.isEmpty(_appId))
 			throw new Exception("appId 不能为空");
 		Activity _activity = DoServiceContainer.getPageViewFactory().getAppContext();
@@ -238,41 +246,42 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 			_scriptEngine.callback(_callbackFuncName, _invokeResult);
 		}
 	}
-	
+
 	@Override
 	public void shareToQQ(JSONObject _dictParas, final DoIScriptEngine _scriptEngine, final String _callbackFuncName) throws Exception {
-		String _appId = DoJsonHelper.getString(_dictParas,"appId", "");
+		activity = _scriptEngine.getCurrentPage().getPageView();
+		activity.registActivityResultListener(this);
+		String _appId = DoJsonHelper.getString(_dictParas, "appId", "");
 		if (TextUtils.isEmpty(_appId))
 			throw new Exception("appId 不能为空");
 		final Activity _activity = DoServiceContainer.getPageViewFactory().getAppContext();
 		if (mTencent == null) {
 			mTencent = Tencent.createInstance(_appId, _activity.getApplicationContext());
 		}
-		
-		int _shareType = DoJsonHelper.getInt(_dictParas,"type", 0); // 分享的类型  0：默认，图文分享；1：纯图分享，只支持本地图；2：音乐分享；3：应用分享
-		String _title = DoJsonHelper.getString(_dictParas,"title", ""); //标题  分享的标题, 最长30个字符
-		String _targetUrl = DoJsonHelper.getString(_dictParas,"url", "");    //目标地址  分享后点击文本后打开的地址
-		String _imageUrl = DoJsonHelper.getString(_dictParas,"image", "");  //图片地址  分享后显示的图片
-		String _summary = DoJsonHelper.getString(_dictParas,"summary", "");   //摘要  分享的消息摘要，最长40个字
-		String _audioUrl = DoJsonHelper.getString(_dictParas,"audio", "");  //音乐文件的远程链接   音乐文件的远程链接, 以URL的形式传入, 不支持本地音乐
-		String _appName = DoJsonHelper.getString(_dictParas,"appName", "");    //应用名称
-		
-		if(TextUtils.isEmpty(_title)){
+
+		int _shareType = DoJsonHelper.getInt(_dictParas, "type", 0); // 分享的类型  0：默认，图文分享；1：纯图分享，只支持本地图；2：音乐分享；3：应用分享
+		String _title = DoJsonHelper.getString(_dictParas, "title", ""); //标题  分享的标题, 最长30个字符
+		String _targetUrl = DoJsonHelper.getString(_dictParas, "url", ""); //目标地址  分享后点击文本后打开的地址
+		String _imageUrl = DoJsonHelper.getString(_dictParas, "image", ""); //图片地址  分享后显示的图片
+		String _summary = DoJsonHelper.getString(_dictParas, "summary", ""); //摘要  分享的消息摘要，最长40个字
+		String _audioUrl = DoJsonHelper.getString(_dictParas, "audio", ""); //音乐文件的远程链接   音乐文件的远程链接, 以URL的形式传入, 不支持本地音乐
+		String _appName = DoJsonHelper.getString(_dictParas, "appName", ""); //应用名称
+
+		if (TextUtils.isEmpty(_title)) {
 			_title = "share title";
 		}
-		
-		
+
 		final Bundle params = new Bundle();
 		switch (_shareType) {
 		case 1:
 			params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_IMAGE);
 			if (null == DoIOHelper.getHttpUrlPath(_imageUrl)) {
 				_imageUrl = DoIOHelper.getLocalFileFullPath(_scriptEngine.getCurrentApp(), _imageUrl);
-			}else{
+			} else {
 				throw new Exception("纯图分享，只支持选择本地图片");
 			}
-		    if(TextUtils.isEmpty(_imageUrl)){
-		    	throw new Exception("纯图分享，图片不能为空");
+			if (TextUtils.isEmpty(_imageUrl)) {
+				throw new Exception("纯图分享，图片不能为空");
 			}
 			params.putString(QQShare.SHARE_TO_QQ_IMAGE_LOCAL_URL, _imageUrl);
 			break;
@@ -290,7 +299,7 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 		case 3:
 			params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_APP);
 			params.putString(QQShare.SHARE_TO_QQ_TITLE, _title);
-			params.putString(QQShare.SHARE_TO_QQ_SUMMARY,_summary);
+			params.putString(QQShare.SHARE_TO_QQ_SUMMARY, _summary);
 			if (null == DoIOHelper.getHttpUrlPath(_imageUrl)) {
 				_imageUrl = DoIOHelper.getLocalFileFullPath(_scriptEngine.getCurrentApp(), _imageUrl);
 			}
@@ -308,7 +317,8 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 			break;
 		}
 		params.putString(QQShare.SHARE_TO_QQ_APP_NAME, _appName);
-		
+		doIScriptEngine = _scriptEngine;
+		callbackFuncName = _callbackFuncName;
 		// QQ分享要在主线程做
 		ThreadManager.getMainHandler().post(new Runnable() {
 			@Override
@@ -319,22 +329,21 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 				}
 			}
 		});
+
 	}
-	
-	
-	private String checkUrl(String _targetUrl) throws Exception{
-		if(TextUtils.isEmpty(_targetUrl)){
+
+	private String checkUrl(String _targetUrl) throws Exception {
+		if (TextUtils.isEmpty(_targetUrl)) {
 			return "http://www.deviceone.net";
 		}
-		if(null == DoIOHelper.getHttpUrlPath(_targetUrl)){ //没有包含http:// 开头
+		if (null == DoIOHelper.getHttpUrlPath(_targetUrl)) { //没有包含http:// 开头
 			_targetUrl = "http://" + _targetUrl;
 		}
 		return _targetUrl;
 	}
-	
-	
-	private class QQShareListener implements IUiListener{
-		
+
+	private class QQShareListener implements IUiListener {
+
 		private DoIScriptEngine scriptEngine;
 		private String callbackFuncName;
 		private DoInvokeResult invokeResult;
@@ -347,13 +356,15 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 
 		@Override
 		public void onCancel() {
-	
+
 		}
 
 		@Override
 		public void onComplete(Object arg0) {
+
 			invokeResult.setResultBoolean(true);
 			scriptEngine.callback(callbackFuncName, invokeResult);
+			activity.unregistActivityResultListener(activityResultListener);
 		}
 
 		@Override
@@ -362,24 +373,28 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 			scriptEngine.callback(callbackFuncName, invokeResult);
 		}
 	}
-	 
+
 	@Override
 	public void shareToQzone(JSONObject _dictParas, final DoIScriptEngine _scriptEngine, final String _callbackFuncName) throws Exception {
-		String _appId = DoJsonHelper.getString(_dictParas,"appId", "");
+		activity = _scriptEngine.getCurrentPage().getPageView();
+		activity.registActivityResultListener(this);
+		doIScriptEngine = _scriptEngine;
+		callbackFuncName = _callbackFuncName;
+		String _appId = DoJsonHelper.getString(_dictParas, "appId", "");
 		if (TextUtils.isEmpty(_appId))
 			throw new Exception("appId 不能为空");
 		final Activity _activity = DoServiceContainer.getPageViewFactory().getAppContext();
 		if (mTencent == null) {
 			mTencent = Tencent.createInstance(_appId, _activity.getApplicationContext());
 		}
-		
-		int _shareType = DoJsonHelper.getInt(_dictParas,"type", 0); // 分享的类型  0：默认，图文分享；1：应用分享
-		String _title = DoJsonHelper.getString(_dictParas,"title", ""); //标题  分享的标题, 最长200个字符
-		String _targetUrl = DoJsonHelper.getString(_dictParas,"url", "");    //目标地址  分享后点击文本后打开的地址
-		String _imageUrl = DoJsonHelper.getString(_dictParas,"image", "");  //图片地址  分享后显示的图片
-		String _summary = DoJsonHelper.getString(_dictParas,"summary", "");   //摘要  分享的消息摘要，最长600个字
-		
-		if(TextUtils.isEmpty(_title)){
+
+		int _shareType = DoJsonHelper.getInt(_dictParas, "type", 0); // 分享的类型  0：默认，图文分享；1：应用分享
+		String _title = DoJsonHelper.getString(_dictParas, "title", ""); //标题  分享的标题, 最长200个字符
+		String _targetUrl = DoJsonHelper.getString(_dictParas, "url", ""); //目标地址  分享后点击文本后打开的地址
+		String _imageUrl = DoJsonHelper.getString(_dictParas, "image", ""); //图片地址  分享后显示的图片
+		String _summary = DoJsonHelper.getString(_dictParas, "summary", ""); //摘要  分享的消息摘要，最长600个字
+
+		if (TextUtils.isEmpty(_title)) {
 			_title = "share title";
 		}
 		final Bundle params = new Bundle();
@@ -391,20 +406,20 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 			params.putInt(QzoneShare.SHARE_TO_QZONE_KEY_TYPE, QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT);
 			break;
 		}
-		
+
 		params.putString(QzoneShare.SHARE_TO_QQ_TITLE, _title);
 		params.putString(QzoneShare.SHARE_TO_QQ_TARGET_URL, checkUrl(_targetUrl));
 		ArrayList<String> _images = new ArrayList<String>();
 		if (null == DoIOHelper.getHttpUrlPath(_imageUrl)) {
 			_imageUrl = DoIOHelper.getLocalFileFullPath(_scriptEngine.getCurrentApp(), _imageUrl);
 		}
-	    if(TextUtils.isEmpty(_imageUrl)){
-	    	throw new Exception("分享的图片不能为空");
+		if (TextUtils.isEmpty(_imageUrl)) {
+			throw new Exception("分享的图片不能为空");
 		}
 		_images.add(_imageUrl);
 		params.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, _images);
 		params.putString(QzoneShare.SHARE_TO_QQ_SUMMARY, _summary);
-		
+
 		// QZone分享要在主线程做
 		ThreadManager.getMainHandler().post(new Runnable() {
 			@Override
@@ -414,5 +429,14 @@ public class do_TencentQQ_Model extends DoSingletonModule implements do_TencentQ
 				}
 			}
 		});
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == Constants.REQUEST_QQ_SHARE) {
+			Tencent.onActivityResultData(requestCode, resultCode, intent, new QQShareListener(doIScriptEngine, callbackFuncName));
+		} else if (requestCode == Constants.REQUEST_QZONE_SHARE) {
+			Tencent.onActivityResultData(requestCode, resultCode, intent, new QQShareListener(doIScriptEngine, callbackFuncName));
+		}
 	}
 }
